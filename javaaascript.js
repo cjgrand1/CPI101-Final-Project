f1639b1a171b9e756d1f4e471dc0d913cd0266b2
cjgrand1 <c.grande@cox.net>
2020-11-16 12:01:00 -0700

    Added Javascript for the pop up window added to product page

--- javaaascript.js
+++ javaaascript.js
@@ -10,3 +10,23 @@ function addDropShadow() {
 		header.classList.remove("header-shadow");
 	}
 }
+
+var modal = document.getElementById("ARmodal");
+
+var btn = document.getElementById("ARbtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+	modal.style.display ="block";
+}
+
+span.onclick = function() {
+	modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
